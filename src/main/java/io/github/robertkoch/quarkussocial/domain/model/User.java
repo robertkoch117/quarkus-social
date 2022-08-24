79c22c955f6183b96ea39d032e76fcb024bcc147
Robert Koch <robertkoch117@gmail.com>
2022-08-24 08:06:02 -0300

    first commit - Concluindo as operacoes CRUD

---
+++ src/main/java/io/github/robertkoch/quarkussocial/domain/model/User.java
@@ -0,0 +1,69 @@
+package io.github.robertkoch.quarkussocial.domain.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+@Table(name="users")
+public class User extends PanacheEntityBase{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="age")
+	private Integer age;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(age, other.age) && Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+	
+}
---
+++ src/main/java/io/github/robertkoch/quarkussocial/rest/UserResource.java
@@ -0,0 +1,71 @@
+package io.github.robertkoch.quarkussocial.rest;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.github.robertkoch.quarkussocial.domain.model.User;
+import io.github.robertkoch.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+	@POST
+	@Transactional
+	public Response createUser(CreateUserRequest userRequest) {
+		User user = new User();
+		user.setAge(userRequest.getAge());
+		user.setName(userRequest.getName());
+		
+		user.persist();
+		
+		return Response.ok(user).build();
+	}
+	
+	@GET
+	public Response listAllUsers() {
+		PanacheQuery<User> query = User.findAll();
+		return Response.ok(query.list()).build();
+	}
+	
+	@DELETE
+	@Path("{id}")
+	@Transactional
+	public Response deleteUser(@PathParam("id") Long id) {
+		User user = User.findById(id);
+		
+		if(user != null) {
+			user.delete();
+			return Response.ok().build();
+		}
+		
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
+	
+	@PUT
+	@Path("{id}")
+	@Transactional
+	public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData) {
+		User user = User.findById(id);
+		
+		if(user != null) {
+			user.setName(userData.getName());
+			user.setAge(userData.getAge());
+			return Response.ok().build();
+		}
+		
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
+}
