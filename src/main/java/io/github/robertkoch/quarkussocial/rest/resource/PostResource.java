0389ea58b940b61adc2fb705fabfaf42a9d33db8
Robert Koch <robertkoch117@gmail.com>
2022-08-25 20:14:01 -0300

    Restrigindo o acesso aos posts

--- src/main/java/io/github/robertkoch/quarkussocial/rest/resource/PostResource.java
+++ src/main/java/io/github/robertkoch/quarkussocial/rest/resource/PostResource.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,13 +17,13 @@ import javax.ws.rs.core.Response;
 
 import io.github.robertkoch.quarkussocial.domain.model.Post;
 import io.github.robertkoch.quarkussocial.domain.model.User;
+import io.github.robertkoch.quarkussocial.rest.domain.repository.FollowerRepository;
 import io.github.robertkoch.quarkussocial.rest.domain.repository.PostRepository;
 import io.github.robertkoch.quarkussocial.rest.domain.repository.UserRepository;
 import io.github.robertkoch.quarkussocial.rest.dto.CreatePostRequest;
 import io.github.robertkoch.quarkussocial.rest.dto.PostResponse;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Sort;
-import io.quarkus.panache.common.Sort.Direction;
 
 @Path("/users/{userId}/posts")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -31,11 +32,13 @@ public class PostResource {
 	
 	private UserRepository userRepository;
 	private PostRepository postRepository;
+	private FollowerRepository followerRepository;
 
 	@Inject
-	public PostResource(UserRepository userRepository, PostRepository postRepository) {
+	public PostResource(UserRepository userRepository, PostRepository postRepository, FollowerRepository followerRepository) {
 		this.userRepository = userRepository;
 		this.postRepository = postRepository;
+		this.followerRepository = followerRepository;
 	}
 	
 	@POST
@@ -56,11 +59,22 @@ public class PostResource {
 	}
 	
 	@GET
-	public Response listPosts(@PathParam("userId") Long userId) {
+	public Response listPosts(@PathParam("userId") Long userId, @HeaderParam("followerId") Long followerId) {
 		User user = userRepository.findById(userId);
 		
 		if(user == null) {
 			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		
+		User follower = userRepository.findById(followerId);
+		
+		if(follower == null) {
+			return Response.status(Response.Status.BAD_REQUEST).entity("Inexistent followerId").build();
+		}
+		
+		boolean follows = followerRepository.follows(follower, user);
+		if(!follows) {
+			return Response.status(Response.Status.FORBIDDEN).entity("You can't see these posts").build();
 		}
 		
 		PanacheQuery<Post> query = postRepository.find("user", Sort.by("dateTime",Sort.Direction.Descending), user);
