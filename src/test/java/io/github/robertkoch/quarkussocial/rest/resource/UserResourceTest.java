16746bef8b2327bd1bece5b84940d63d446cd8b7
Robert Koch <robertkoch117@gmail.com>
2022-08-26 09:45:51 -0300

    Testando o caso de erro de validação

--- src/test/java/io/github/robertkoch/quarkussocial/rest/resource/UserResourceTest.java
+++ src/test/java/io/github/robertkoch/quarkussocial/rest/resource/UserResourceTest.java
@@ -1,12 +1,17 @@
 package io.github.robertkoch.quarkussocial.rest.resource;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.github.robertkoch.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.robertkoch.quarkussocial.rest.dto.exception.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -37,5 +42,31 @@ class UserResourceTest {
 		
 		assertEquals(201, response.statusCode());
 		assertNotNull(response.jsonPath().getString("id"));
+	}
+	
+	@Test
+	@DisplayName("Should return error when json is not valid")
+	public void createUserValidationErrorTest() {
+		CreateUserRequest user = new CreateUserRequest();
+		user.setAge(null);
+		user.setName(null);
+		
+		Response response = 
+				given()
+					.contentType(ContentType.JSON)
+					.body(user)
+				.when()
+					.post("/users")
+				.then()
+					.extract().response();
+		
+		assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+		assertEquals("Validation Error", response.jsonPath().getString("message"));
+		
+		List<Map<String, String>> errors = response.jsonPath().getList("errors");
+		assertNotNull(errors.get(0).get("message"));
+		assertNotNull(errors.get(1).get("message"));
+//		assertEquals("Age is Required", errors.get(0).get("message"));
+//		assertEquals("Name is Required", errors.get(1).get("message"));
 	}
 }
